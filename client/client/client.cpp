d2db0c72f052d1dd7e2d44433994960d5a768c4a
Alexander <Alexander-1082@mail.ru>
2020-11-01 20:43:42 +0700

    Реализован базовый механизм работы с сокетами

--- client/client/client.cpp
+++ client/client/client.cpp
@@ -1,24 +1,106 @@
 ﻿#include <iostream>
 #include <WinSock2.h>
+#include <stdio.h>
+#include <ws2tcpip.h>
+
+#pragma warning(disable:4996)
 
 // Need to link with Ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
+#define DEFAULT_BUFLEN 512
 
 int main()
 {
-    int err, sockfd;
-    WSAData wsaData;
-    err = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    char sendbuf[] = "Client: sending data test";
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+    std::string port = "8008";
+    std::string host = "127.0.0.1";
+    if (result != NO_ERROR) {
+        printf("WSAStartup function failed with error: %d\n", result);
+        return 1;
+    }
+    
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("socket function failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+    /*
+    sockaddr_in clientService;
+    clientService.sin_family = AF_INET;
+    clientService.sin_addr.s_addr = inet_addr("176.59.131.101");
+    clientService.sin_port = htons(8008);*/
 
-    if (err != 0) {
-        printf("WSAStartup failed with error: %d\n", err);
+    //inet_pton(AF_INET, "176.59.131.101", &clientService.sin_addr);
+    struct addrinfo hints;
+    struct addrinfo* servinfo;  // will point to the results
+
+    memset(&hints, 0, sizeof hints); // make sure the struct is empty
+    hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+    hints.ai_flags = AI_PASSIVE;
+    getaddrinfo(host.c_str(), port.c_str(), &hints, &servinfo); //192.168.43.100
+    //176.59.131.101
+    result = connect(ConnectSocket, servinfo->ai_addr, servinfo->ai_addrlen);
+
+    //result = connect(ConnectSocket, (SOCKADDR*)&clientService, sizeof(clientService));
+    if (result == SOCKET_ERROR) {
+        printf("connect function failed with error: %ld\n", WSAGetLastError());
+        result = closesocket(ConnectSocket);
+        if (result == SOCKET_ERROR)
+            printf("closesocket function failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
         return 1;
     }
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        printf("ERROR opening socket");
+    printf("Connected to server.\n");
 
+    //Делаем общение с сервером
+
+    result = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+    if (result == SOCKET_ERROR) {
+        printf("send failed with error: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Bytes Sent: %ld\n", result);
+
+    result = shutdown(ConnectSocket, SD_SEND);
+    if (result == SOCKET_ERROR) {
+        printf("shutdown failed: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    do {
+
+        result = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+        if (result > 0)
+            printf("Bytes received: %d\n", result);
+        else if (result == 0)
+            printf("Connection closed\n");
+        else
+            printf("recv failed: %d\n", WSAGetLastError());
+
+    } while (result > 0);
+
+    result = closesocket(ConnectSocket);
+    if (result == SOCKET_ERROR) {
+        printf("closesocket function failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    freeaddrinfo(servinfo);
     WSACleanup();
     std::cout << "Hello World!\n";
+    getchar();
 }
--- server/server/server.cpp
+++ server/server/server.cpp
@@ -1,24 +1,114 @@
 ﻿#include <iostream>
 #include <WinSock2.h>
+#include <ws2tcpip.h>
+//#pragma warning(disable:4996)
 
 // Need to link with Ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
 
+#define DEFAULT_BUFLEN 512  
+
 int main()
 {
-    int err, sockfd;
+    int result, ListenSocket;
+    char recvbuf[DEFAULT_BUFLEN];
+    char sendbuf[] = "this is a test";
+    int recvbuflen = DEFAULT_BUFLEN;
+    u_short port = 8008;
     WSAData wsaData;
-    err = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    //sockaddr_in server;
+    result = WSAStartup(MAKEWORD(2, 2), &wsaData);
     
-    if (err != 0) {
-        printf("WSAStartup failed with error: %d\n", err);
+    if (result != 0) {
+        printf("WSAStartup failed with error: %d\n", result);
         return 1;
     }
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    ListenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (ListenSocket < 0)
         printf("ERROR opening socket");
+    /*
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY; //inet_addr("176.59.139.3");
+    server.sin_port = htons(port);
+    std::cout << server.sin_port << std::endl;*/
 
+    struct addrinfo hints;
+    struct addrinfo* servinfo;  // will point to the results
+
+    memset(&hints, 0, sizeof hints); // make sure the struct is empty
+    hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+    hints.ai_flags = AI_NUMERICHOST;
+    getaddrinfo("127.0.0.1", "8008", &hints, &servinfo);
+
+    //result = bind(ListenSocket, (SOCKADDR*)&server, sizeof(server));
+    result = bind(ListenSocket, servinfo->ai_addr, servinfo->ai_addrlen);
+
+
+    if (result == SOCKET_ERROR) {
+        printf("bind failed with error %u\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    result = listen(ListenSocket, 1);
+
+    if (result == SOCKET_ERROR) {
+        printf("listen failed with error: %ld\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+    
+    SOCKET AcceptSocket;
+    printf("Waiting for client to connect...\n");
+
+    AcceptSocket = accept(ListenSocket, NULL, NULL);
+    if (AcceptSocket == INVALID_SOCKET) {
+        printf("accept failed with error: %ld\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+    else
+        printf("Client connected.\n");
+
+    result = send(AcceptSocket, sendbuf, (int)strlen(sendbuf), 0);
+    if (result == SOCKET_ERROR) {
+        printf("send failed: %d\n", WSAGetLastError());
+        closesocket(AcceptSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Bytes Sent: %ld\n", result);
+
+    result = shutdown(AcceptSocket, SD_SEND);
+    if (result == SOCKET_ERROR) {
+        printf("shutdown failed: %d\n", WSAGetLastError());
+        closesocket(AcceptSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    do {
+
+        result = recv(AcceptSocket, recvbuf, recvbuflen, 0);
+        if (result > 0)
+            printf("Bytes received: %d\n", result);
+        else if (result == 0)
+            printf("Connection closed\n");
+        else
+            printf("recv failed: %d\n", WSAGetLastError());
+
+    } while (result > 0);
+
+    closesocket(ListenSocket);
+
+    freeaddrinfo(servinfo);
     WSACleanup();
     std::cout << "Hello World!\n";
+    getchar();
 }
